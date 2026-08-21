9fcf8f3469527cc284e78baf8639990c449824c2
강민서 <rachel5640@naver.com>
2026-08-22 02:37:56 +0900

    feat: 메타데이터 입력

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,47 @@
+import styled from 'styled-components';
+
 import InfiniteGallery from '../components/InfiniteGallery';
 import Preloader from '../components/Preloader';
+import { introText } from '../data/intro';
 
 const MainPage = () => {
   return (
     <>
       <Preloader />
+      <IntroText>
+        <p>{introText}</p>
+      </IntroText>
+
       <InfiniteGallery />
     </>
   );
 };
+
+const IntroText = styled.div`
+  position: fixed;
+  top: 2rem;
+  left: 2rem;
+  z-index: 500;
+  max-width: 26vw;
+  pointer-events: none;
+
+  ${({ theme }) => theme.fonts.Text01};
+
+  line-height: 1.5;
+  color: ${({ theme }) => theme.colors.black};
+
+  font-weight: 900;
+
+  p {
+    margin-top: 0.6rem;
+    font-size: 1rem;
+  }
+
+  @media (max-width: 720px) {
+    max-width: calc(100vw - 10rem);
+    font-size: 1rem;
+    top: 1rem;
+  }
+`;
 
 export default MainPage;
